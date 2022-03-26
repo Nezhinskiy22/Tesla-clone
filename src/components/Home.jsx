7978faef27aabbfa293f342f53262ac58a0130eb
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-03-26 21:30:22 +0200

    Sections

---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+const Home = () => {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lower Cost Solar Panels in America"
+        description=""
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  height: 100vh;
+`;
+
+export default Home;
